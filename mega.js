8bc111351f1560bd720ccb0d8a5153fcf22456c2
Matrix-Zat <godswaydoe761@gmail.com>
2025-05-08 22:04:16 +0000

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,14 +1,14 @@
 const mega = require('megajs');
 
-const email = 'matrixzat99@gmail.com'; // <-- Your Mega.nz email
-const password = 'Godpapa@1999';       // <-- Your Mega.nz password
+const email = 'matrixzat99@gmail.com'; // Your Mega.nz email
+const password = 'Godpapa@1999';       // Your Mega.nz password
 
 function upload(stream, filename) {
   return new Promise((resolve, reject) => {
     const storage = mega({ email, password });
 
     storage.on('ready', () => {
-      const file = storage.upload({ name: filename });
+      const file = storage.upload({ name: filename, allowUploadBuffering: true }); // This avoids the file size error!
       stream.pipe(file);
 
       file.on('complete', () => {
@@ -26,4 +26,3 @@ function upload(stream, filename) {
 }
 
 module.exports = { upload };
-
